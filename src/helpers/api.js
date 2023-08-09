d5ba578da36a97f3d58c02d4826e2fb5155a9cca
Your Name <you@example.com>
2023-08-09 11:12:36 +0200

    create api file for spotify endpoints

---
+++ src/helpers/api.js
@@ -0,0 +1,45 @@
+import config from "../config";
+import axiosInstance from "./axiosInterceptor";
+
+// API paths
+const apiPaths = {
+  newReleases: `${config.api.baseUrl}/browse/new-releases`,
+  featuredPlaylists: `${config.api.baseUrl}/browse/featured-playlists`,
+  browseGenres: `${config.api.baseUrl}/browse/categories`,
+};
+
+// Fetch and display Released This Week songs
+export async function fetchReleasedThisWeek() {
+  try {
+    const response = await axiosInstance.get(apiPaths.newReleases);
+    const releasedThisWeekSongs = response.data.albums.items;
+    console.log("Released This Week songs:", releasedThisWeekSongs);
+    return releasedThisWeekSongs;
+  } catch (error) {
+    console.error("Error fetching Released This Week songs:", error);
+  }
+}
+
+// Fetch and display Featured Playlists
+export async function fetchFeaturedPlaylists() {
+  try {
+    const response = await axiosInstance.get(apiPaths.featuredPlaylists);
+    const featuredPlaylists = response.data.playlists.items;
+    console.log("Featured Playlists:", featuredPlaylists);
+    return featuredPlaylists;
+  } catch (error) {
+    console.error("Error fetching Featured Playlists:", error);
+  }
+}
+
+// Fetch and display Browse genres
+export async function fetchBrowseGenres() {
+  try {
+    const response = await axiosInstance.get(apiPaths.browseGenres);
+    const browseGenres = response.data.categories.items;
+    console.log("Browse genres:", browseGenres);
+    return browseGenres;
+  } catch (error) {
+    console.error("Error fetching Browse genres:", error);
+  }
+}
